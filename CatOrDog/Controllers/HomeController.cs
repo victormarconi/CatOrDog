1a28549a7508e3cc48494ec180a4d8534d8b480a
victormarconi <victormarconis@gmail.com>
2023-11-05 20:14:35 -0300

    Adicionar arquivos de projeto.

---
+++ CatOrDog/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace CatOrDog.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // Método para obter imagem de cachorro
+        [HttpGet]
+        public async Task<IActionResult> GetDogImage()
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetStringAsync("https://api.thedogapi.com/v1/images/search");
+                var data = JArray.Parse(response);
+                var imageUrl = data[0]["url"].ToString();
+                return Json(new { url = imageUrl });
+            }
+        }
+
+        // Método para obter imagem de gato
+        [HttpGet]
+        public async Task<IActionResult> GetCatImage()
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetStringAsync("https://api.thecatapi.com/v1/images/search");
+                var data = JArray.Parse(response);
+                var imageUrl = data[0]["url"].ToString();
+                return Json(new { url = imageUrl });
+            }
+        }
+    }
+}
---
+++ CatOrDog/wwwroot/js/site.js
@@ -0,0 +1,21 @@
+﻿document.getElementById('dog-button').addEventListener('click', function () {
+    fetch('/Home/GetDogImage')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('animal-image').src = data.url;
+        })
+        .catch(error => {
+            console.error('Erro ao buscar a imagem de cachorro:', error);
+        });
+});
+
+document.getElementById('cat-button').addEventListener('click', function () {
+    fetch('/Home/GetCatImage')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('animal-image').src = data.url;
+        })
+        .catch(error => {
+            console.error('Erro ao buscar a imagem de gato:', error);
+        });
+});
